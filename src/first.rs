b6d7d3816668f7f38608b7ffd52e90c3e1fbb785
Tim Abell <tim@timwise.co.uk>
2022-08-25 18:48:53 +0100

    fail at push
    https://rust-unofficial.github.io/too-many-lists/first-push.html

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,4 @@
+//  list -> head -> link -> box<node> -> node -> elem+list -> head ...
 pub struct List {
     head: Link,
 }
@@ -15,5 +16,13 @@ enum Link {
 impl List {
     fn new() -> Self {
         List { head: Link::Empty }
+    }
+    // before: LIST^1 -> head^1 -> link^1 -> more<box<node^1>> -> node^1 -> elem^1 + next^1 -> list^2 -> head^2 ...
+    // after:  LIST^1 -> head^1 -> link^1 -> more<box<node^3>> -> node^3 -> elem^3 + next^3 -> list^1 -> head^1 -> link^1 -> more<box<node^1>> ...
+    fn push(&mut self, elem: i32){
+        let node = Node{
+            elem,
+            next: self.head,
+        };
     }
 }
