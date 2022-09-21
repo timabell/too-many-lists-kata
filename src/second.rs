f910595f89b39af603b2d62c81fbc726be267e0f
Tim Abell <tim@timwise.co.uk>
2022-09-21 18:55:56 +0100

    Remove now redundant Link prefixes

--- src/second.rs
+++ src/second.rs
@@ -22,21 +22,21 @@ struct Node {
 
 impl List {
     fn new() -> Self {
-        List { head: Link::None }
+        List { head: None }
     }
     // before: LIST^1 -> head^1 -> link^1 -> more<box<node^1>> -> node^1 -> elem^1 + next^1 -> list^2 -> head^2 ...
     // after:  LIST^1 -> head^1 -> link^1 -> more<box<node^3>> -> node^3 -> elem^3 + next^3 -> list^1 -> head^1 -> link^1 -> more<box<node^1>> ...
     fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem,
-            next: mem::replace(&mut self.head, Link::None),
+            next: mem::replace(&mut self.head, None),
         });
-        self.head = Link::Some(new_node);
+        self.head = Some(new_node);
     }
     pub fn pop(&mut self) -> Option<i32> {
-        match mem::replace(&mut self.head, Link::None) {
-            Link::None => None,
-            Link::Some(node) => {
+        match mem::replace(&mut self.head, None) {
+            None => None,
+            Some(node) => {
                 self.head = node.next;
                 Some(node.elem)
             }
